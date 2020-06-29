4d0bdad357130664e1d758935413e8d8af30ef81
sayhellotoraji <51025595+sayhellotoraji@users.noreply.github.com>
2020-06-29 21:53:11 +0530

    Spring JWT

---
+++ VyntraMart/VyntraMart-Backend/src/main/java/com/rajasekar/vyntramart/springjwt/repository/RoleRepository.java
@@ -0,0 +1,15 @@
+package com.rajasekar.vyntramart.springjwt.repository;
+
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rajasekar.vyntramart.springjwt.models.ERole;
+import com.rajasekar.vyntramart.springjwt.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+	Optional<Role> findByName(ERole name);
+}
